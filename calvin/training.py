4ec57e315ab99a059b0961d88fbc028942362960
Oier Mees <oier.mees@gmail.com>
2021-09-04 00:08:34 +0200

    ddp sharded

--- calvin/training.py
+++ calvin/training.py
@@ -56,7 +56,7 @@ def wrap_train(config_name):
         if is_multi_gpu_training(trainer_args["gpus"]):  # type: ignore
             trainer_args["accelerator"] = "ddp"
             trainer_args["plugins"] = DDPPlugin(find_unused_parameters=False)
-            # trainer_args["plugins"] = "ddp_sharded"
+            trainer_args["plugins"] = "ddp_sharded"
 
             if not cfg.slurm:
                 modify_argv_hydra()
