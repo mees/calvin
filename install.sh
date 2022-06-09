83c08a2ac1134854c31d578c58f253a2c0358462
Adrian Röfer <aroefer@uni-bremen.de>
2022-06-09 12:33:33 +0200

    Added pre-install step for wheel and cmake 3.18.4

--- install.sh
+++ install.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Wheel is never depended on, but always needed. MulticoreTSNE requires lower CMake version
+pip install wheel cmake==3.18.4
+
 cd calvin_env/tacto
 pip install -e .
 cd ..
