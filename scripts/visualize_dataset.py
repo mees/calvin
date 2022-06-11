2bca61880c83d8b64f72626af980a4b0be207d11
Adrian Röfer <aroefer@uni-bremen.de>
2022-06-11 14:13:50 +0200

    Added print of language annotation. Fixed BGR view.

--- scripts/visualize_dataset.py
+++ scripts/visualize_dataset.py
@@ -15,26 +15,37 @@ if __name__ == '__main__':
         print(f'Path {args.path} is either not a directory, or does not exist.')
         exit()
 
-    files = sorted(Path(args.path).glob('episode_*.npz'))
+    indices = next(iter(np.load(f'{args.path}/scene_info.npy', allow_pickle=True).item().values()))
+    indices = list(range(indices[0], indices[1] + 1))
+    
+    annotations = np.load(f'{args.path}/lang_annotations/auto_lang_ann.npy', allow_pickle=True).item()
+    annotations = list(zip(annotations['info']['indx'], annotations['language']['ann']))
 
     idx = 0
+    ann_idx = -1
 
     while True:
-        t = np.load(files[idx], allow_pickle=True)
+        t = np.load(f'{args.path}/episode_{indices[idx]:07d}.npz', allow_pickle=True)
 
         for d in args.data:
             if d not in t:
                 print(f'Data {d} cannot be found in transition')
                 continue
 
-            cv2.imshow(d, t[d])
+            cv2.imshow(d, t[d][:,:,::-1])
+
+        for n, ((low, high), ann) in enumerate(annotations):
+            if indices[idx] >= low and indices[idx] <= high:
+                if n != ann_idx:
+                    print(f'{ann}')
+                    ann_idx = n
 
         key = cv2.waitKey(0)
         if key == ord('q'):
             break
         elif key == 83:  # Right arrow
-            idx = (idx + 1) % len(files)
+            idx = (idx + 1) % len(indices)
         elif key == 81:  # Left arrow
-            idx = (len(files) + idx - 1) % len(files)
+            idx = (len(indices) + idx - 1) % len(indices)
         else:
             print(f'Unrecognized keycode "{key}"')
