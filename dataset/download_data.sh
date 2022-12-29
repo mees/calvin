ea9174d09b56432a947fa5c607a2bc2455ab6e48
Oier Mees <oier.mees@gmail.com>
2022-12-29 08:58:32 +0100

    do not delete dataset zip files by default, to allow verifying the zips with checksums

--- dataset/download_data.sh
+++ dataset/download_data.sh
@@ -6,14 +6,14 @@ then
 
     echo "Downloading task_D_D ..."
     wget http://calvin.cs.uni-freiburg.de/dataset/task_D_D.zip
-    unzip task_D_D.zip && rm task_D_D.zip
+    unzip task_D_D.zip
     echo "saved folder: task_D_D"
 elif [ "$1" = "ABC" ]
 then
 
     echo "Downloading task_ABC_D ..."
     wget http://calvin.cs.uni-freiburg.de/dataset/task_ABC_D.zip
-    unzip task_ABC_D.zip && rm task_ABC_D.zip
+    unzip task_ABC_D.zip
     echo "saved folder: task_ABC_D"
 
 elif [ "$1" = "ABCD" ]
@@ -21,7 +21,7 @@ then
 
     echo "Downloading task_ABCD_D ..."
     wget http://calvin.cs.uni-freiburg.de/dataset/task_ABCD_D.zip
-    unzip task_ABCD_D.zip && rm task_ABCD_D.zip
+    unzip task_ABCD_D.zip
     echo "saved folder: task_ABCD_D"
 
 elif [ "$1" = "debug" ]
@@ -29,7 +29,7 @@ then
 
     echo "Downloading debug dataset ..."
     wget http://calvin.cs.uni-freiburg.de/dataset/calvin_debug_dataset.zip
-    unzip calvin_debug_dataset.zip && rm calvin_debug_dataset.zip
+    unzip calvin_debug_dataset.zip
     echo "saved folder: calvin_debug_dataset"
 
 
